2241a4b5c179926ad1bfb61712040e006859db6a
Keun Mo Koo <rnrmsah.koo@gmail.com>
2021-08-08 17:30:23 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,45 @@
-import React, { Component } from 'react';
-import {CardList}  from './components/card-list/card-list-component.jsx';
-import {SearchBox} from './components/search-box/search-box.component.jsx';
-import axios from 'axios';
-import {GlobalStyle} from './global.styles'
- 
-import './App.css';
-// import Data from './pages/homepage/info.json'
+import React from "react";
+import axios from "axios";
 
+import "./App.css";
 
-class App extends React.Component{
-  constructor(){
-    super()
+class App extends React.Component {
+  constructor() {
+    super();
 
-    this.state ={
+    this.state = {
       products: [],
-      searchField: ''
-      
-    }
-    this.handleChange = this.handleChange.bind(this)
+      searchField: "",
+    };
   }
 
-  handleChange(e){
-    this.setState({searchField:e.target.value})
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
   }
- 
-  componentDidMount(){
-   
-    axios.get(`/info.json`)
-    .then(res => {
-      console.log(res)
-      this.setState({products:res.data})
-    })
-  
-    
-  }
-  render(){
-    
-    const { products, searchField} = this.state
-    console.log(this.state)
-    
-    const filteredProducts = products.filter(function(product){
-      return (product.productName.toLowerCase().includes(searchField.toLowerCase()) || product.price.includes(searchField))
-    })
 
- 
-    return(
+  componentDidMount() {
+    axios.get(`/info.json`).then((res) => {
+      console.log(res);
+      this.setState({ products: res.data });
+    });
+  }
+  render() {
+    const { products, searchField } = this.state;
+    console.log(this.state);
+
+    const filteredProducts = products.filter(function (product) {
+      return (
+        product.productName.toLowerCase().includes(searchField.toLowerCase()) ||
+        product.price.includes(searchField)
+      );
+    });
+
+    return (
       <div className="App">
-      <GlobalStyle/>
-      <h1> Products </h1>
-      <SearchBox 
-        placeholder = 'Search Product'
-        handleChange = {this.handleChange}
-        />
-      <CardList products={filteredProducts}/>
-      
-
-    </div>
-    )
+        <h1> Heading</h1>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
